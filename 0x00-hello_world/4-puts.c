f62e9046849cbb69b83f4148a88b5a04155d5a86
ihioko01 <maryihioko@gmail.com>
2023-07-13 13:00:21 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* Main –Entry point
+*
+* Return: Always 0 (Success)
+*/
+Int main(void)
+{
+		puts(“/”Programming is like building a multilingual puzzle”) ;
+		return (0) ;
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* Main –Entry point
+*
+* Return: Always 0 (Success)
+*/
+Int main(void)
+{
+		Printf(“with proper grammar, but the outcome is a piece of art , /n”);
+		Return (0);
+}
+
