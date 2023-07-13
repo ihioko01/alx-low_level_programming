1cc2debe2a9eed1212f81b22be72173049e45ad4
ihioko01 <maryihioko@gmail.com>
2023-07-13 16:45:56 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,17 @@
 #include <unistd.h>
 
-int main(void) 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
+int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
-	return 1;
+       	ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+	ssize_t ret = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+	if (ret != len)
+        return 1;
+
+    return 1;
 }
 
