0fedc5aadcc456758930a41958910261651ff327
ihioko01 <maryihioko@gmail.com>
2023-07-13 15:22:05 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
-* Main –Entry point
-*
-* Return: Always 0 (Success)
+ * main - Entry point
+ * Description: operation
+ * Return: Always 0 (Success)
 */
-Int main(void)
+int main(void)
 {
-		Printf(“with proper grammar, but the outcome is a piece of art , /n”);
-		Return (0);
-}
 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 /**
-* main  - Entry point
-*
-* Return Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
 */
-Int main(void)
+int main(void)
 {
-	Printf(“Size of a char: %d byte(s)\n”, sizeof(char));
-	Printf(“Size of an int: %d byte(s)\n”, sizeof(int));
-	Printf(“Size of a long int: %d byte(s)\n”, sizeof(long int));
-	Printf(“Size of a long long int: %d byte(s)\n”, sizeof(long long int));
-	Printf(“Size of a float: %d byte(s)\n”, sizeof(float));
-	Return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size4 of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
 
