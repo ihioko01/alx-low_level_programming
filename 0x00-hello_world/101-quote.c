eef64dd120d12068fc59b5b7005c5458a471bea6
ihioko01 <maryihioko@gmail.com>
2023-07-13 16:29:37 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return 1 (Success)
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora) Korpar, 2015-10-19\n stdout);
+	fput("and that piece of art is useful\" - Dora) Korpar, 2015-10-19\n stdout");
 	return (1);
 }
