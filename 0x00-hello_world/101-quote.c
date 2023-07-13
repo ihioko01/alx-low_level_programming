d150fa4caceb4bc28c195ce72e0c52ba578f7cb4
ihioko01 <maryihioko@gmail.com>
2023-07-13 16:34:44 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - Entry point
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-	fput("and that piece of art is useful\" - Dora) Korpar, 2015-10-19\n stdout");
-	return (1);
+
+int main(void) {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return 1;
 }
+
