16928d35a242c8741a828e2a7723d3468fb9e9c1
ihioko01 <maryihioko@gmail.com>
2023-07-13 13:46:20 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* Main –Entry point
-*
-* Return: Always 0 (Success)
+ * Main –Entry point
+ *
+  * Return: 0 (Success)
 */
-Int main(void)
+int main(void)
 {
-		puts(“/”Programming is like building a multilingual puzzle”) ;
-		return (0) ;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
