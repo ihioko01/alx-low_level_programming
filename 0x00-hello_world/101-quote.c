18b8225fe6adecfb3ffb329d3027bcc00c14ac26
ihioko01 <maryihioko@gmail.com>
2023-07-13 17:20:03 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 	ssize_t ret = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
 
 	if (ret != len)
-	return (1);
+        return (1);
 
 	return (1);
 }
