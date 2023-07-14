1c449560a8e86531e421cb4843c78dbbffa710c7
ihioko01 <maryihioko@gmail.com>
2023-07-14 16:24:18 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,30 +1,29 @@
-#include <unistd.h>
-
 /**
- * _putchar - Writes a character to the standard output
- * @c: The character to write
- *
- * Return: On success, returns the number of bytes written.
- *         On error, returns -1 and sets errno appropriately.
- */
-int _putchar(char c)
+* _putchar - Writes a character to the standard output
+* @c: The character to write
+*
+* Return: On success, returns the number of bytes written.
+*         On error, returns -1 and sets errno appropriately.
+*/
+int _putchar(int c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
 /**
- * main - Entry point of the program
- *
- * Return: Always returns 0 to indicate successful execution
- */
+* main - Entry point of the program
+*
+* Return: Always returns 0 to indicate successful execution
+*/
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i < 10; i++)
-		_putchar(i + '0');
+for (i = 0; i < 10; i++)
+_putchar(i + 48); /* Add 48 to convert the integer to its ASCII representation*/
 
-	_putchar('\n');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
+
