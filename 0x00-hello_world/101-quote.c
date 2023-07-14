2961b837d65f804ef504d94bc9ca0ede0d49eb05
ihioko01 <maryihioko@gmail.com>
2023-07-13 17:25:12 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,7 @@
  */
 int main(void)
 {
-	ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-	ssize_t ret = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
-
-	if (ret != len)
-        return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout); 
 
 	return (1);
 }
