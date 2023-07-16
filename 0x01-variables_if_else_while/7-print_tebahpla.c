a142df4206fd9feb52f7c11a728e1ed66fea2b2c
ihioko01 <maryihioko@gmail.com>
2023-07-16 13:12:47 -0700

    7-print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,4 @@
-#include <unistd.h>
-
-/**
-* _putchar - Writes a character to the standard output (stdout)
-* @c: The character to be written
-*
-* Return: On success, 1 is returned. On error, -1 is returned,
-*         and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#include <stdio.h>
 
 /**
 * main - Entry point of the program
@@ -23,10 +11,10 @@ char c;
 
 for (c = 'z'; c >= 'a'; c--)
 {
-_putchar(c);
+putchar(c);
 }
 
-_putchar('\n');
+putchar('\n');
 
 return (0);
 }
