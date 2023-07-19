421c7565e2e15fd239a2d9501d5a599c6d7402bb
ihioko01 <maryihioko@gmail.com>
2023-07-18 17:35:33 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,42 +3,24 @@
 
 /**
 * print_to_98 - Prints all natural numbers from n to 98
-* @n: The number ti start printing from
-* Return: Always 0
+* @n: The starting number
 */
-
 void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (; n <= 98; n++)
+for (; n < 98; n++)
 {
-if (n ==98)
-{
-printf ("%d", n);
-printf ("\n");
-break;
-{
-else
-{
-printf ("%d, ", n);
-}
+printf("%d, ", n);
 }
 }
 else
 {
-for (; n >= 98; n--)
+for (; n > 98; n--)
 {
-if (n == 98)
-{
-printf ("%d", n);
-printf ("\n");
-break;
-}
-else
-{
-printf ("%d, ", n);
+printf("%d, ", n);
 }
 }
+printf("%d\n", n);
 }
-}
+
