9a4fa2aacdc9a00da52e5cc848031b01e4ab9f44
ihioko01 <maryihioko@gmail.com>
2023-09-05 13:41:06 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,11 +13,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 char *buffer
 ssize_t fd, bw, br;
 fd = open(filename, O_RDONLY);
-if (fd == -1)
+if (fd == NULL)
 return (0);
 buffer = malloc(size of(char) * letters);
+if (buffer == NULL)
+{
+close(fd);
+return (0);
+}
 bw = write(STDOUT_FILENO, buffer, br);
+if (bw == -1 || bw != br)
+{
+free(buffer);
+close(fd);
+return (0);
+}
 br = read(fd, buffer, letters);
+if (br == -1)
+{
+fre(buffer);
+close(fd);
+return (0);
+}
 
 free(buffer);
 close(fd);
