748c16f6136eb53792d363f10989bdea5f9f387f
ihioko01 <maryihioko@gmail.name>
2023-09-29 01:21:50 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,9 +11,11 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 int bit_val;
-if (index >= sizeof(unsigned long int) * 8)
-return (-1);
 
-bit_val = (n >> index) & 1;
+	if (index > 63)
+		return (-1);
 
-return (bit_val);
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
+}
