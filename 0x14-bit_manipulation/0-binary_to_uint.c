a4cc752e0439d93fd05827293c727515268a60ec
ihioko01 <maryihioko@gmail.name>
2023-09-29 00:20:45 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,24 +6,23 @@
 * @b: A pointer to a string containing '0' and '1' characters.
 *
 * Return: The converted unsigned int, or 0 if:
-*         - b is NULL
-*         - There is one or more chars in the string b that are not '0' or '1'.
+* b is NULL,
+* There is one or more chars in the string b that are not '0' or '1'.
 */
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int result = 0;
+unsigned int dec_val = 0;
 
-if (!b)
+if (b == NULL)
 return (0);
 
 for (i = 0; b[i]; i++)
 {
-
-if (*b != '0' && *b != '1')
+if (b[i] < '0' || b[i] > '1')
 return (0);
 dec_val = 2 * dec_val + (b[i] - '0');
-
 }
+
 return (dec_val);
 }
