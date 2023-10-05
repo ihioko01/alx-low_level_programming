2fe034a3da35b7706fc0d1fafc78242b744d6c08
ihioko01 <maryihioko@gmail.name>
2023-10-05 13:14:19 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -6,37 +7,33 @@
 * @filename: text file being read
 * @letters: number of letters to be read
 * Return: w- actual number of bytes read and printed
-* 0 when function fails or filename is NULL.
+*        0 when function fails or filename is NULL.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buffer;
-ssize_t fd, bw, br;
+int fd;
+ssize_t w;
+ssize_t t;
+
+if (filename == NULL)
+return (0);
+
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
+
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
-br = read(fd, buffer, letters);
-if (br == -1)
-{
-free(buffer);
-close(fd);
-return (0);
-}
-bw = write(STDOUT_FILENO, buffer, br);
-if (bw == -1 || bw != br)
-{
-free(buffer);
-close(fd);
-return (0);
-}
+
+t = read(fd, buffer, letters);
+w = write(STDOUT_FILENO, buffer, t);
 
 free(buffer);
 close(fd);
-return (bw);
+return (w);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,33 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* create_file - Create a file and write a text content on it.
-* @filename: The name of the file to be created.
-* @text_content: A null terminated string to be written to the fie.
-* Return: 1 on sucess and -1 on failure
+* create_file - Creates a file.
+* @filename: A pointer to the name of the file to create.
+* @text_content: A pointer to a string to write to the file.
+*
+* Return: If the function fails - -1.
+*         Otherwise - 1.
 */
 int create_file(const char *filename, char *text_content)
 {
-int fd, tc, len = 0;
+int fd, write_result, len = 0;
 if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len] != '\0' ; len++)
-;
+for (len = 0; text_content[len];)
+len++;
 }
 
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-if (fd == -1)
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+write_result = write(fd, text_content, len);
+
+if (fd == -1 || write_result == -1)
 return (-1);
 
-tc = write(fd, text_content, len);
-if (tc == -1)
-{
 close(fd);
-return (-1);
-}
 
-close(fd);
 return (1);
 }
