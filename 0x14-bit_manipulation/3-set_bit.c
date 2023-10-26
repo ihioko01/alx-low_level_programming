3727615136e41b7b138b217d948ea691472d8606
ihioko01 <maryihioko@gmail.com>
2023-10-26 12:42:36 -0700

    bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,12 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned long int setbit;
+
 if (index >= (sizeof(unsigned long int) * 8))
 {
 return (-1);
-/* index out of range */
+
 }
 
 unsigned long int setbit = 1UL << index;
