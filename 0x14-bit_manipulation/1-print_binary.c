d727d83367a447bc22cf094b430e26c7178e175f
ihioko01 <maryihioko@gmail.com>
2023-10-26 12:53:35 -0700

    bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,51 @@
 #include "main.h"
 
 /**
-* print_binary - Prints the binary representation of a number.
-* @n: The number to print in binary.
+* _pow - func calculates (base ^ power)
+* @base: base of the exponent
+* @power: power of the exponent
+*
+* Return: value of (base ^ power)
+*/
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+unsigned long int num;
+unsigned int a;
+
+num = 1;
+for (a = 1; a <= power; a++)
+num *= base;
+return (num);
+}
+
+/**
+* print_binary - prints a number in binary notation
+* @n: number to print
+*
+* Return: void
 */
 void print_binary(unsigned long int n)
 {
-int outflag = 0;
+unsigned long int divisor, check;
+char flag;
 
-if (n == 0)
+flag = 0;
+divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (divisor != 0)
+{
+check = n & divisor;
+if (check == divisor)
+{
+flag = 1;
+_putchar('1');
+}
+else if (flag == 1 || divisor == 1)
 {
 _putchar('0');
-return;
+}
+divisor >>= 1;
+}
 }
 
-for (unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-mask > 0;
-mask >>= 1)
-{
-if (n & mask || outflag)
-{
-_putchar((n & mask) ? '1' : '0');
-outflag = 1;
-}
 }
 }
