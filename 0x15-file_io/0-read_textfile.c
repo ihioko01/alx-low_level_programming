231a04e7481a7a8fcfcef8e4dfc94b25ff506ebd
ihioko01 <maryihioko@gmail.com>
2023-10-31 05:52:29 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,21 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t _putchar(char c);
 
 /**
-* read_textfile- Read text file print to STDOUT.
-* @filename: text file being read
-* @letters: number of letters to be read
-* Return: w- actual number of bytes read and printed
-*        0 when function fails or filename is NULL.
+* read_textfile - Reads and prints a text file to standard output.
+* @filename: Name of the file to read.
+* @letters: The number of letters to read and print.
+*
+* Return: The actual number of letters read and printed. 0 on failure.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buffer;
 int fd;
-ssize_t w;
-ssize_t t;
+ssize_t r, w;
+char *buffer;
 
 if (filename == NULL)
 return (0);
@@ -23,17 +24,36 @@ fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 
-buffer = malloc(sizeof(char) * letters);
+buffer = malloc(letters);
 if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
 
-t = read(fd, buffer, letters);
-w = write(STDOUT_FILENO, buffer, t);
-
-free(buffer);
+r = read(fd, buffer, letters);
+if (r == -1)
+{
 close(fd);
+free(buffer);
+return (0);
+}
+
+w = 0;
+while (w < r)
+{
+if (_putchar(buffer[w]) == -1)
+{
+close(fd);
+free(buffer);
+return (0);
+}
+w++;
+}
+
+close(fd);
+free(buffer);
+
 return (w);
 }
+
